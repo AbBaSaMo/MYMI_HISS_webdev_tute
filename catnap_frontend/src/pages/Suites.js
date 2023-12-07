048d1ba6e51b1baa035c62fb0194f2938f54870b
Baaset Moslih <s.a.baaset.moslih@gmail.com>
2023-12-07 17:10:25 +1100

    feat: added suites details in Suites.js as array

--- catnap_frontend/src/pages/Suites.js
+++ catnap_frontend/src/pages/Suites.js
@@ -1,10 +1,106 @@
+import {Box, Text, VStack} from "@chakra-ui/react";
 
 const Suites = () => {
 
+    const suites = [
+        {
+            imgSrc: "Cage_Standard-1920w.webp",
+            suiteName: "Standard Suite",
+            description: "Standard suites are great for older cats " +
+                "or cats that don't do a lot of jumping " +
+                "around. They have two levels with a " +
+                "raised sleeping area with a special mesh " +
+                "shelf that is great for sleeping on.",
+            costDescription: [
+                {
+                    value: "$22",
+                    unit: "per day for one cat"
+                }
+            ]
+        }, {
+            imgSrc: "Cage_Executive-1920w.webp",
+            suiteName: "Executive Suite",
+            description: "Large and spacious, the executive suite " +
+                "has four different levels giving our guest " +
+                "room to climb and explore. " +
+                "With views of the gardens and places to hide,",
+            costDescription: [
+                {
+                    value: "$24",
+                    unit: "per day for one cat"
+                }, {
+                    value: "$19",
+                    unit: "for each additional cat"
+                }
+            ]
+        }, {
+            imgSrc: "Cage_HiRise-1920w.webp",
+            suiteName: "High Rise Suite",
+            description: "The high rise suite stands 4 meters with " +
+                "nine levels for your cat to " +
+                "explore. Perfect for the cat that likes to " +
+                "climb and see out over the entire garden.",
+            costDescription: [
+                {
+                    value: "$26",
+                    unit: "per day for one cat"
+                }, {
+                    value: "$19",
+                    unit: "for each additional cat"
+                }
+            ]
+        }, {
+            imgSrc: "Cage_GardenSuite-1920w.webp",
+            suiteName: "Garden Suite",
+            description: "Our most popular suite of all. Perfect for " +
+                "two cars or one active cat. The garden " +
+                "suite is two rooms connected by an " +
+                "overhead walk way, One half is in the " +
+                "garden court yard, so they can come out " +
+                "and see the birds up close. Both sides " +
+                "have bedding.",
+            costDescription: [
+                {
+                    value: "$28",
+                    unit: "per day for one cat"
+                }, {
+                    value: "$19",
+                    unit: "for each additional cat"
+                }
+            ]
+        }, {
+            imgSrc: "Presidential_Solid-1920w.webp",
+            suiteName: "Presidential Suite",
+            description: "The presidential comes with a people " +
+                "sized bed with electric blanket, a couch, " +
+                "doonas, pillows and lots of climbing " +
+                "areas and stairs to explore. Perfect for " +
+                "two or more cats or one very pampered kitty.",
+            costDescription: [
+                {
+                    value: "$32",
+                    unit: "per day for one cat"
+                }, {
+                    value: "$19",
+                    unit: "for each additional cat"
+                }
+            ]
+        }
+    ]
+
     return (
-        <div>
-            We offer this
-        </div>
+        <VStack>
+            <Box w='100%' h='40px'/>
+            <Text fontSize='3xl' as='b'>Our Suites</Text>
+            <hr/>
+            <Text textAlign='center' as='b'>
+                We also offer seven types of accommodation, designed to cater for cat needs as<br/>
+                well as offering different standards of accommodation.<br/>
+                Discounts for multiple cats to be lodged at Catnap Cattery are offered!
+            </Text>
+            <hr/>
+
+        </VStack>
     )
 }
 
